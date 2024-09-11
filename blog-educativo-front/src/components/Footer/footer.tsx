9497a73f1b2c2f52d57b1b1d1c78d6248d235613
Priscyla Stefanea Moura Figueiredo <priscylastefanea@gmail.com>
2024-09-11 14:51:02 -0300

    removendo node_modules

---
+++ blog-educativo-front/src/components/Footer/footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './footer.css'
+
+const Footer: React.FC = () => {
+    return (
+        <footer className="footer">
+            <p>Direitos autorais 2024</p>
+        </footer>
+    );
+};
+
+export default Footer
